ca0245a708cb360d3caf337c28318e8eb7fe1796
Oz Klingel <ozkl@amdocs.com>
2023-10-31 14:27:42 +0200

    arrange 2

--- lib/src/views/primary/pages/chatBox/supportScreen.dart
+++ lib/src/views/primary/pages/chatBox/supportScreen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:hadar_program/src/services/routing/go_router_provider.dart';
 import 'package:hadar_program/src/views/primary/pages/chatBox/successDialog.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../../../services/notifications/toaster.dart';
+import '../../../../services/routing/go_router_provider.dart';
 import 'dropDownWidget.dart';
 import 'errorDialog.dart';
 
@@ -31,156 +30,136 @@ class SupportScreen extends ConsumerWidget {
     MediaQueryData queryData;
     queryData = MediaQuery.of(context);
     return Scaffold(
-        body: ListView(
-      children: <Widget>[
-        SizedBox(
-          height: 25,
-        ),
-        Container(
-          color: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 18),
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.topRight,
-                child: GestureDetector(
-                  child: Icon(
-                    Icons.arrow_back,
-                    color: Colors.black,
-                  ),
-                  onTap: () => const HomeRouteData().go(context),
-                ),
+        appBar: AppBar(
+            centerTitle: true,
+            leading: GestureDetector(
+              child: Icon(
+                Icons.arrow_back,
+                color: Colors.black,
               ),
-              Align(
-                alignment: Alignment.topCenter,
-                child: GestureDetector(
-                  child: const Text(
-                    'פתיחת פנייה',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  onTap: () => Toaster.unimplemented(),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Container(
-          color: Colors.white,
-          height: queryData.size.height - 100,
-          padding: const EdgeInsets.all(32),
-          /*1*/
-          child: Column(
-            children: [
-              /*2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: const Text(
-                  'שליחת הודעה',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-              ),
-              Text(
-                'אנא השאירו את פנייתכם ונחזור אליכם ',
-                style: TextStyle(
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+              onTap: () => const HomeRouteData().go(context),
+            ),
+            title: Text("פתיחת פניה")),
+        body: SingleChildScrollView(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.all(32),
+              /*1*/
+              child: Column(
                 children: [
-                  Padding(
-                    padding: EdgeInsets.all(3.0),
+                  /*2*/
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text(
+                      'שליחת הודעה',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
                   ),
                   Text(
-                    "נושא",
-                    style: TextStyle(color: Colors.black, fontSize: 14),
+                    'אנא השאירו את פנייתכם ונחזור אליכם ',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
                   ),
-                  const Text('*', style: TextStyle(color: Colors.red)),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(3.0),
+                      ),
+                      Text(
+                        "נושא",
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                      ),
+                      const Text('*', style: TextStyle(color: Colors.red)),
+                    ],
+                  ),
+                  DropdownButtonExample(),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(3.0),
+                      ),
+                      Text(
+                        "תוכן הפניה",
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                      ),
+                      const Text('*', style: TextStyle(color: Colors.red)),
+                    ],
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    child: TextFormField(
+                      textAlign: TextAlign.start,
+                      textAlignVertical: TextAlignVertical.top,
+                      maxLength: 250,
+                      controller: _myController,
+                      minLines: 7,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 7,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "error";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Container(
+                      height: 50,
+                      alignment: Alignment.bottomCenter,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: StadiumBorder(),
+                          primary: (_myController.text.isEmpty)
+                              ? Color.fromRGBO(236, 242, 245, 1)
+                              : Color(sendButtonColor),
+                          minimumSize: const Size.fromHeight(50), // NEW
+                        ),
+                        onPressed: () async {
+                          SupportScreen.contant = _myController.text;
+                          print(SupportScreen.contant);
+                          String result = "";
+                          if (SupportScreen.contant != "" &&
+                              SupportScreen.subject != null) {
+                            result =
+                                "success"; //await HttpService.ChatBoxUrl(SupportScreen.contant,SupportScreen.subject,context);
+                          }
+                          print(result);
+                          if (result == "success") {
+                            showFancyCustomDialog(context);
+                          } else {
+                            showAlertDialog(context);
+                          }
+                        },
+                        child: Text(
+                          "שליחת פנייה",
+                          style: TextStyle(
+                              color: _myController.text.isEmpty
+                                  ? Colors.grey
+                                  : Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                      ))
                 ],
               ),
-              DropdownButtonExample(),
-              SizedBox(
-                height: 40,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(3.0),
-                  ),
-                  Text(
-                    "תוכן הפניה",
-                    style: TextStyle(color: Colors.black, fontSize: 14),
-                  ),
-                  const Text('*', style: TextStyle(color: Colors.red)),
-                ],
-              ),
-              Container(
-                alignment: Alignment.center,
-                child: TextFormField(
-                  textAlign: TextAlign.start,
-                  textAlignVertical: TextAlignVertical.top,
-                  maxLength: 250,
-                  controller: _myController,
-                  minLines: 7,
-                  keyboardType: TextInputType.multiline,
-                  maxLines: 10,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "error";
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Spacer(),
-              Container(
-                  height: 50,
-                  alignment: Alignment.bottomCenter,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: StadiumBorder(),
-                      primary: (_myController.text.isEmpty)
-                          ? Color.fromRGBO(236, 242, 245, 1)
-                          : Color(sendButtonColor),
-                      minimumSize: const Size.fromHeight(50), // NEW
-                    ),
-                    onPressed: () async {
-                      SupportScreen.contant = _myController.text;
-                      print(SupportScreen.contant);
-                      String result = "";
-                      if (SupportScreen.contant != "" &&
-                          SupportScreen.subject != null) {
-                        result =
-                            "success"; //await HttpService.ChatBoxUrl(SupportScreen.contant,SupportScreen.subject,context);
-                      }
-                      print(result);
-                      if (result == "success") {
-                        showFancyCustomDialog(context);
-                      } else {
-                        showAlertDialog(context);
-                      }
-                    },
-                    child: Text(
-                      "שליחת פנייה",
-                      style: TextStyle(
-                          color: _myController.text.isEmpty
-                              ? Colors.grey
-                              : Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                  ))
-            ],
-          ),
-        )
-      ],
-    ));
+            )
+          ],
+        )));
   }
 }
