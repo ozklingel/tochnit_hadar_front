751e7eac53f14a9f7aac83fb3ed2d6567d501d6a
Agon Noga <git@noga.dev>
2024-04-16 14:30:59 +0300

    fix lint

--- lib/src/views/primary/pages/notifications/views/Notification_details_screen.dart
+++ lib/src/views/primary/pages/notifications/views/Notification_details_screen.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hadar_program/src/models/auth/auth.dto.dart';
 import 'package:hadar_program/src/models/notification/notification.dto.dart';
-import 'package:hadar_program/src/services/auth/auth_service.dart';
 import 'package:hadar_program/src/views/primary/pages/notifications/controller/notifications_controller.dart';
-import 'package:hadar_program/src/views/primary/pages/notifications/views/widgets/notification_widget.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class NotificationDetailsScreen extends HookConsumerWidget {
@@ -30,11 +27,10 @@ class NotificationDetailsScreen extends HookConsumerWidget {
 
     useEffect(
       () {
-        
-          ref
-              .read(notificationsControllerProvider.notifier)
-              .setToReadStatus(message);
-        
+        ref
+            .read(notificationsControllerProvider.notifier)
+            .setToReadStatus(message);
+
         return null;
       },
       [],
@@ -45,30 +41,26 @@ class NotificationDetailsScreen extends HookConsumerWidget {
         title: const Text('פרטי התראה'),
         centerTitle: true,
       ),
-      body:  Padding(
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 232,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                         Text(
-                          message.description
-                        ),
-                   ],
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: 232,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    message.description,
                   ),
-                ),
-              
-              ],
+                ],
+              ),
             ),
-          ),
-      
-    
-  
+          ],
+        ),
+      ),
     );
   }
 }
