fe179e8ee416feb4ea9d026731febeb2bbb34633
Agon Noga <git@noga.dev>
2024-04-26 19:01:23 +0300

    Sbs9TK8q

--- lib/src/views/primary/pages/messages/views/message_details_screen.dart
+++ lib/src/views/primary/pages/messages/views/message_details_screen.dart
@@ -30,11 +30,7 @@ class MessageDetailsScreen extends HookConsumerWidget {
 
     useEffect(
       () {
-        if (auth.role == UserRole.melave) {
-          ref
-              .read(messagesControllerProvider.notifier)
-              .setToReadStatus(message);
-        }
+        ref.read(messagesControllerProvider.notifier).setToReadStatus(message);
 
         return null;
       },
