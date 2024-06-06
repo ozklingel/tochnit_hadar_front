a3bfd7867c77ed27e15a56ac3429c647cf51f00a
Agon Noga <git@noga.dev>
2024-06-06 13:34:11 +0300

    minor io platform related fix

--- lib/src/services/api/export_import/upload_file.dart
+++ lib/src/services/api/export_import/upload_file.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:hadar_program/src/core/constants/consts.dart';
 import 'package:hadar_program/src/services/networking/dio_service/dio_service.dart';
 import 'package:http_parser/http_parser.dart';
@@ -32,13 +33,20 @@ class UploadFile extends _$UploadFile {
     //     )
 
     final formData = FormData.fromMap({
-      // file.path is not supported on web, so we use fromBytes instead:
+      // NOTE(yamhoresh): file.path is not supported on web, so we use fromBytes instead:
       // https://github.com/miguelpruivo/flutter_file_picker/wiki/FAQ
-      "file": MultipartFile.fromBytes(
-        file.bytes as List<int>,
-        filename: file.name,
-        contentType: MediaType.parse('multipart/form-data'),
-      ),
+      // NOTE(noga-dev): why do we care about web support?
+      "file": kIsWeb
+          ? MultipartFile.fromBytes(
+              file.bytes as List<int>,
+              filename: file.name,
+              contentType: MediaType.parse('multipart/form-data'),
+            )
+          : await MultipartFile.fromFile(
+              file.path!,
+              filename: file.name,
+              contentType: MediaType.parse('multipart/form-data'),
+            ),
     });
 
     final result = await ref.watch(dioServiceProvider).post(
